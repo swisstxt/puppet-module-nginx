7126987c835c67eb2f7f015487db16364ab6e0fb
Rene Moser <rene.moser@swisstxt.ch>
2013-08-23 16:27:33 +0200

    updated nginx so it will be usable for nginx_pcache

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,15 +1,22 @@
-class nginx {
-  package{'nginx':
+class nginx (
+  $nginx_package = 'nginx',
+  $site_classes = undef
+){
+  package{$nginx_package:
     ensure => installed,
+    alias  => 'nginx',
   }
   service{'nginx':
-    ensure => running,
-    enable => true,
-    hasstatus => false, 
-    require => Package['nginx'],
+    ensure    => running,
+    enable    => true,
+    hasstatus => false,
+    require   => Package['nginx'],
   }
   nginx::configfile{[
     'nginx.conf',
     'mime.types',
   ]:}
+  if $site_classes {
+    class{$site_classes:}
+  }
 }
