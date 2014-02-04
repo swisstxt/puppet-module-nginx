9f4b29175e9acb38bedbbac8f888e1782a18f873
Simon Josi <simon.josi@swisstxt.ch>
2014-02-04 15:27:25 +0100

    add purge_vhosts parameter: purge unmanaged hosts if true

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,6 +5,7 @@ class nginx (
     'nginx.conf',
     'mime.types',
   ],
+  $purge_vhosts = false,
 ) {
   package { $package:
     ensure => present,
@@ -14,6 +15,12 @@ class nginx (
     ensure    => running,
     enable    => true,
     hasstatus => false,
+  }
+  if $purge_vhosts {
+    file { '/etc/nginx/conf.d':
+      recurse => true,
+      purge   => true,
+    }
   }
   ::nginx::configfile { $manage:
     source_module => $caller_module_name,
